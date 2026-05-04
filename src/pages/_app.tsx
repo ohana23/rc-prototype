d60c1e90b1ad98d6addd054c02ce054dfd695f33
Danny <danny.ohana@gmail.com>
2026-05-04 12:59:08 -0400

    clean up -- back to basics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Agentation } from "agentation";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      {process.env.NODE_ENV === "development" && <Agentation />}
+    </>
+  );
 }
